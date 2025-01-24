0b5e69e2d386c7ce4fe78871ca7a8c5374a5c4cb
ZainulArkaanAlinsi <zainaril13@gmail.com>
2025-01-24 15:03:46 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ["Poppins", "serif"],
+        'jakarta': ["Plus Jakarta Sans", "serif"],
+      },
+      colors: {
+        background: "#1F1E25",
+        manggo: "#FED049",
+        ungu: "#A12568",
+         gradieant: "#A12568",
+         putih: "#FFFFFF",
+      },
+
+      padding: {
+        30: "120px",
+      }
+    },
+
+  },
+  plugins: [],
+}
+
